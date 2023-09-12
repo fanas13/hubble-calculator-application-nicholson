17f48f6d2a802a94761be0ab23b9cf0c2c560f97
Nicholson Galela <nicholsongalela@gmail,com>
2023-09-12 23:18:42 +0200

    added functionality on calculator and styling so it is appealing to the user

--- src/routes/FeatureRoutes/Home.jsx
+++ src/routes/FeatureRoutes/Home.jsx
@@ -1,7 +1,32 @@
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
 
 const Home = ({props}) => {
+    const [expression, setExpression] = React.useState('');
+    const [result, setResult] = React.useState('');
+
+    const handleClick = (value) => {
+        setExpression((prevExpression) => prevExpression + value);
+    };
+  
+    const clearInput = () => {
+        setExpression('');
+        setResult('');
+    };
+  
+    const calculateResult = () => {
+        try {
+            setResult(eval(expression));
+        } catch (error) {
+            setResult('Error');
+        }
+    };
+
     return(
         <Grid
             container
@@ -9,13 +34,110 @@ const Home = ({props}) => {
             alignItems="center"     // Center vertically
         >
             <Toolbar />
-            <Grid
-                md={7}
-                sx={{ display: { xs: 'none', sm: 'block' } }}
-                container
-            >
-                <Grid md={12} fullWidth>
-                    content goes here
+            <Grid md={4} sx={{ display: { sm: 'block', marginTop: '25px' }}}>
+                <Grid md={10} fullWidth style={{ padding: '40px', textAlign: 'center' }}>
+                    <Paper elevation={1} style={{ padding: '40px', textAlign: 'center' }}>
+                        <TextField
+                            label="Expression"
+                            variant="outlined"
+                            type="number"
+                            fullWidth
+                            value={expression}
+                            onChange={(e) => setExpression(e.target.value)}
+                        />
+                        <Grid container spacing={2} style={{ marginTop: '20px' }}>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" onClick={() => handleClick('+')}>
+                                    +
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" onClick={() => handleClick('-')}>
+                                    -
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" onClick={() => handleClick('*')}>
+                                    *
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" onClick={() => handleClick('/')}>
+                                    /
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('1')}>
+                                    1
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('2')}>
+                                    2
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('3')}>
+                                    3
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" onClick={() => handleClick('4')}>
+                                    4
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('5')}>
+                                    5
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('6')}>
+                                    6
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('7')}>
+                                    7
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('8')}>
+                                    8
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" onClick={() => handleClick('9')}>
+                                    9
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" onClick={() => handleClick('0')}>
+                                    0
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" onClick={() => handleClick('.')}>
+                                    .
+                                </Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button variant="contained" color="primary" disabled={!expression ? true : false}onClick={calculateResult}>
+                                    =
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button fullWidth variant="contained" disabled={(!expression && !result ) ? true : false} onClick={clearInput}>
+                                    Clear
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    <Paper elevation={1} style={{ padding: '40px', textAlign: 'center', marginTop: '40px' }}>
+                        <Typography variant="h6">
+                            Result: {result}
+                        </Typography>
+                    </Paper>
                 </Grid>
             </Grid>
         </Grid>

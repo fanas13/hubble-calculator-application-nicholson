52e8b5cdad367ea43337a60683cb0ed6c630eb54
Nicholson Galela <nicholsongalela@gmail,com>
2023-09-12 21:49:17 +0200

    installed the missing mui dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import theme from './config/theme';
+import Home from './routes/FeatureRoutes/Home';
 
 function App() {
   return (
     <div className="App">
-      
-    </div>
+      <Home themeConfig={theme} />
+    </div>    
   );
 }
 
